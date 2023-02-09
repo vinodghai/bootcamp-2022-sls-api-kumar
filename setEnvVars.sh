75338e700d8da3e783e67452352849c49e738cbb
Vinod Kumar <vinkumar@dminc.com>
2023-02-09 14:36:44 -0500

    Finish my app

--- setEnvVars.sh
+++ setEnvVars.sh
@@ -1,3 +1,3 @@
-export GITHUB_USERNAME="<<username>>"
-export GITHUB_REPO="bootcamp-2022-sls-api-<<lastname>>"
-export BUCKET_NAME="<<username>>-bootcamp-2022-bucket" #username must be all lowercase
+export GITHUB_USERNAME="vinodghai"
+export GITHUB_REPO="bootcamp-2022-sls-api-kumar"
+export BUCKET_NAME="vinodghai-bootcamp-2022-bucket" #username must be all lowercase
